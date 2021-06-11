7d0cd2d82fc9ea1acbf5a1fa298ed3d7bae70651
תומר ספיר <sapirtomer1989@gmail.com>
2021-06-11 12:22:54 +0300

    corrections

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(__dirname + "/assets"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
